805782de1cbcefa530181066a2475907f32bf97d
Upamanyu Das <upamanyu.das.work@gmail.com>
2021-05-01 00:02:15 +0530

    add favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,7 +8,24 @@ const Layout = () => {
 			<Head>
 				<title>No Save WA</title>
 				<meta name='description' content='NextJS and Tailwind Starter' />
-				<link rel='icon' href='/favicon.ico' />
+				<link
+					rel='apple-touch-icon'
+					sizes='180x180'
+					href='/apple-touch-icon.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='32x32'
+					href='/favicon-32x32.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='16x16'
+					href='/favicon-16x16.png'
+				/>
+				<link rel='manifest' href='/site.webmanifest' />
 			</Head>
 
 			<div
